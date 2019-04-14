a0d9ddcf6df7d7a7d7ebe4a143962c307058af9c
Andrew Waller <andrewhwaller@gmail.com>
2019-04-14 21:52:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,15 @@ class Student
   #  with DB[:conn]
   attr_accessor :name, :grade
   attr_reader :id
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      );
+      SQL
+    DB[:conn].execute(sql) 
+  end
 end
